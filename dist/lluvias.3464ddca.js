001815ed420a74d872339aad378eed437be601af
pitulincode <erikillzon@gmail.com>
2025-06-03 13:38:03 -0500

    modficacion 3

--- dist/lluvias.3464ddca.js
+++ dist/lluvias.3464ddca.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"bRtuo":[function(require,module,exports,__globalThis) {
+})({"6gbjq":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 52216;
+var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"j4kuM":[function(require,module,exports,__globalThis) {
 
-},{}]},["bRtuo","j4kuM"], "j4kuM", "parcelRequire4e56", {})
+},{}]},["6gbjq","j4kuM"], "j4kuM", "parcelRequire4e56", {})
 
 //# sourceMappingURL=lluvias.3464ddca.js.map
